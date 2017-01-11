c3f8ad84b99e63438f1532204dcf796f126cc0e9
Tanvi-Bhandari <tbhandar@informatica.com>
2017-01-11 22:22:42 +0530

    Changes after review comments
    • Moved setJavaForScanners.sh and replaceHostname.sh scripts out of
    template.
    • In order to maintain the AMIs long term, combined all AMIs into a
    single AWSAMIRegionMap.
    • Removed CfnUser resource since we are using Instance Profiles and
    Roles.
    • Created a separate security group just for the RDS DB instance that
    only opens up the SQL port. - InfaDBSecurityGroup.
    • Remove the -e from the shebang at the top of the UserData scripts so
    that script will continue and signal if cfn-init exits with a non-zero
    code.
    • Removed default any CIDR blocks to 0.0.0.0/0

---
+++ scripts/replaceHostname.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -xe
+publicDnsName=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+hostname $publicDnsName
+sudo su -c "sed --in-place '1s/.*/$publicDnsName/' /etc/hostname"
+sudo su -c "echo HOSTNAME=$publicDnsName >> /etc/sysconfig/network"
+sudo su -c "sed -i '$ a preserve_hostname: true' /etc/cloud/cloud.cfg"
