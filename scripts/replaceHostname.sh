15aeaebf27835aad9e86cb089d96088e0d28b77d
Tanvi-Bhandari <tbhandar@informatica.com>
2017-02-14 22:09:25 +0530

    Created New AMIs
    Created new AMIs for both INFAEICADMINHVM and INFAEICCLUSTERHVM.
    Removed the scripts setJavaForScanners.sh and replaceHostname.sh from
    template.
    Added check for sshd is running before signaling wait condition.

--- scripts/replaceHostname.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-publicDnsName=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
-hostname $publicDnsName
-sudo su -c "sed --in-place '1s/.*/$publicDnsName/' /etc/hostname"
-sudo su -c "echo HOSTNAME=$publicDnsName >> /etc/sysconfig/network"
-sudo su -c "sed -i '$ a preserve_hostname: true' /etc/cloud/cloud.cfg"
